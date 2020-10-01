abb50aef6f05e8597751d9dfa8cef1901f2a4649
Germán Moreno <grmmrno@gmail.com>
2020-10-01 19:03:17 -0400

    Finalizando proyecto

---
+++ js/app.js
@@ -0,0 +1,145 @@
+const criptomonedasSelect = document.querySelector("#criptomonedas");
+const monedaSelect = document.querySelector("#moneda");
+const formulario = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
+
+const objBusqueda = {
+    moneda: "",
+    criptomoneda: "",
+};
+
+const obtenerCriptomonedas = (criptomonedas) =>
+    new Promise((resolve) => {
+        resolve(criptomonedas);
+    });
+
+window.onload = () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener("submit", submitFormulario);
+
+    criptomonedasSelect.addEventListener("change", leerValor);
+
+    monedaSelect.addEventListener("change", leerValor);
+};
+
+function consultarCriptomonedas() {
+    const URL =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => obtenerCriptomonedas(data.Data))
+        .then((criptomonedas) => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach((cripto) => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement("option");
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+    });
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+
+    console.log(objBusqueda);
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if (moneda === "" || criptomoneda === "") {
+        mostrarAlerta("Ambos campos son obligatorios");
+        return;
+    }
+
+    consultarAPI();
+}
+
+function consultarAPI() {
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const URL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => mostrarCotizacion(data.DISPLAY[criptomoneda][moneda]));
+}
+
+function mostrarCotizacion(cotizacion) {
+    limpiarHTML();
+
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement("p");
+    precio.classList.add("precio");
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement("p");
+    precioAlto.innerHTML = `Precio más alto del día: <span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement("p");
+    precioBajo.innerHTML = `Precio más bajo del día: <span>${LOWDAY}</span>`;
+
+    const ultimasHoras = document.createElement("p");
+    ultimasHoras.innerHTML = `Últimas 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualizacion = document.createElement("p");
+    ultimaActualizacion.innerHTML = `Última actualización: <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarAlerta(msj) {
+    const existeError = document.querySelector(".error");
+
+    if (!existeError) {
+        const divMensaje = document.createElement("div");
+        divMensaje.classList.add("error");
+
+        divMensaje.textContent = msj;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const Spinner = document.createElement("div");
+    Spinner.classList.add("sk-cube-grid");
+
+    Spinner.innerHTML = `<div class="sk-cube sk-cube1"></div>
+                        <div class="sk-cube sk-cube2"></div>
+                        <div class="sk-cube sk-cube3"></div>
+                        <div class="sk-cube sk-cube4"></div>
+                        <div class="sk-cube sk-cube5"></div>
+                        <div class="sk-cube sk-cube6"></div>
+                        <div class="sk-cube sk-cube7"></div>
+                        <div class="sk-cube sk-cube8"></div>
+                        <div class="sk-cube sk-cube9"></div>`;
+
+    resultado.appendChild(Spinner);
+}
